9c94b669c9f0168347d37044599aa61009eb8692
alk831 <adam54111@gmail.com>
2020-05-14 21:43:35 +0200

    init

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const AboutPage = () => <div>about</div>;
+
+export default AboutPage;
---
+++ src/pages/Index/index.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import css from './index.module.css';
+
+export const HomePage = () => {
+  const [todo, setTodo] = useState<any>();
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/13')
+      .then((response) => response.json())
+      .then(setTodo);
+  }, []);
+
+  return (
+    <div className={css.container}>Hello there {JSON.stringify(todo)}</div>
+  );
+};
+
+export default HomePage;
